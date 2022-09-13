35452494ebfe4aa29759a3d60af3194cc3b09e3b
DKondrov <9177778286@mail.ru>
2022-09-13 15:18:44 +0300

    TextField for slider DONE

---
+++ Colorized/SliderTextField.swift
@@ -0,0 +1,43 @@
+//
+//  SliderTextField.swift
+//  Colorized
+//
+//  Created by D KONDROV on 13.09.2022.
+//
+
+import SwiftUI
+
+struct SliderTextField: View {
+    
+    @Binding var textField: String
+    @Binding var value: Double
+    @State private var showAlert = false
+    
+    var body: some View {
+        
+        TextField("", text: $textField)
+            .textFieldStyle(.roundedBorder)
+            .frame(width: 50)
+            .keyboardType(.decimalPad)
+            .alert(isPresented: $showAlert){
+                Alert(title: Text("Wrong format"), message: Text("Please enter values to 0 from 255"))
+            }
+    }
+}
+
+extension SliderTextField {
+    
+    private func checkValue(){
+        if let value = Int(textField), (0...255).contains(value) {
+            self.value = Double(value)
+            return
+        }
+        showAlert.toggle()
+    }
+}
+
+struct SliderTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderTextField(textField: .constant("128"), value: .constant(128.0))
+    }
+}
