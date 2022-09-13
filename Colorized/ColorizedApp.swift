8777eb815040bd0708008bc694169e7ac66eabb5
DKondrov <9177778286@mail.ru>
2022-09-13 10:31:28 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by D KONDROV on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colorized/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colorized
+//
+//  Created by D KONDROV on 13.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
